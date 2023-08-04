412438c6010d64107054feccae7d615b225bed1b
EItan Barylko <eitan@barylko.com>
2023-08-03 21:39:05 -0700

    Associated the resource from the page with keys that match the expected object

--- tests/cypress/e2e/resources.cy.js
+++ tests/cypress/e2e/resources.cy.js
@@ -4,38 +4,48 @@ import * as R from "ramda";
  * Extracts all the tools on the resources page
  */
 function parseToolsFromPage() {
-return cy.get('.resource')
-    .then((elems) => {
-        return R.pipe(Array.from, R.map(e => {
-            return R.pipe(Array.from, R.take(5), R.map(R.prop("innerText")))(e.children)
-        }))(elems)
-    })
+  return cy.get(".resource").then((elems) => {
+    return R.pipe(
+      Array.from,
+      R.map((e) => {
+        return R.pipe(
+          Array.from,
+          R.take(5),
+          R.map(R.prop("innerText")),
+          R.zipObj(["name", "affiliated", "date", "blurb", "tags"]),
+        )(e.children);
+      }),
+    )(elems);
+  });
 }
 
-const notEmpty = R.complement(R.isEmpty)
+const notEmpty = R.complement(R.isEmpty);
 
-describe('Resources page', () => {
-    describe('When loading the page', () => {
-        it('Displays all the tools', () => {
-            const expected =  [{
-                "name": "ChatGPT",
-                "affiliated": "OpenAI",
-                "date": [2022, 10],
-                "tags": ["Chat", "Research", "Students", "Teachers"],
-                "link": "https://chat.openai.com/",
-                "blurb": "A chat application using OpenAI's GPT LLM.",
-                "description": "An online chat application utilizing OpenAI's GPT large language model (LLM) to understand user inputs and generate outputs."
-            }]
-            cy.intercept('GET', '/resources.json', {
-                statusCode: 200,
-                body: expected,
-            })
-            cy.visit("http://localhost:3000/")
-            cy.get('[data-testid="resources"]').click()
-            const tools = parseToolsFromPage()
-            tools.should('deep.eq', 1 )
-        })
-    })
-})
+describe("Resources page", () => {
+  describe("When loading the page", () => {
+    it("Displays all the tools", () => {
+      const expected = [
+        {
+          name: "ChatGPT",
+          affiliated: "OpenAI",
+          date: [2022, 10],
+          tags: ["Chat", "Research", "Students", "Teachers"],
+          link: "https://chat.openai.com/",
+          blurb: "A chat application using OpenAI's GPT LLM.",
+          description:
+            "An online chat application utilizing OpenAI's GPT large language model (LLM) to understand user inputs and generate outputs.",
+        },
+      ];
+      cy.intercept("GET", "/resources.json", {
+        statusCode: 200,
+        body: expected,
+      });
+      cy.visit("http://localhost:3000/");
+      cy.get('[data-testid="resources"]').click();
+      const tools = parseToolsFromPage();
+      tools.should("deep.eq", 1);
+    });
+  });
+});
 /*
     [expected.name, expected.affiliated, date, expected.blurb, expected.tags]*/
